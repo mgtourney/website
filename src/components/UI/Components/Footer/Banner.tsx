651e95dd459dcfe8469760cd43a643e498e73eef
NightHawk <hasfeldt@yahoo.dk>
2023-03-12 00:23:36 +0100

    fix: :bug: Banner "rel" added, fixed fetch on stats/index, formatting on qualscheck

--- src/components/UI/Components/Footer/Banner.tsx
+++ src/components/UI/Components/Footer/Banner.tsx
@@ -29,11 +29,12 @@ export default function BottomBanner({ visible, headline, message, link, linkTex
                             </div>
                             <div className="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
                                 <a
-                                    href={link || '#'}
-                                    target={"_blank"}
+                                    href={link || "#"}
+                                    target="_blank"
+                                    rel="noreferrer"
                                     className="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-600 bg-white hover:bg-indigo-50"
                                 >
-                                    {linkText || 'Learn more'}
+                                    {linkText || "Learn more"}
                                 </a>
                             </div>
                             <div className="order-2 flex-shrink-0 sm:order-3 sm:ml-2">
--- src/pages/qualscheck.tsx
+++ src/pages/qualscheck.tsx
@@ -38,14 +38,17 @@ export default function Team() {
       }
       if (skillSaberData1.rank + skillSaberData2.rank <= 500) {
         return Error({
-          text: `<b>${skillSaberData1.name}</b></br> <b>${skillSaberData2.name}</b><br> Exceeds the limit by: <br> <b>${ 500 - (skillSaberData1.rank + skillSaberData2.rank)}</b> ranks!`,
+          text: `<b>${skillSaberData1.name}</b></br> <b>${
+            skillSaberData2.name
+          }</b><br> Exceeds the limit by: <br> <b>${
+            500 - (skillSaberData1.rank + skillSaberData2.rank)
+          }</b> ranks!`,
         });
       } else {
         return Success({
           text: `<b>${skillSaberData1.name}</b></br> <b>${skillSaberData2.name}</b><br> can team up!`,
         });
       }
-
     } catch (error) {
       return Error({ text: "Control the ScoreSaber IDs" });
     }
@@ -65,7 +68,8 @@ export default function Team() {
             <div className="px-4 py-5 sm:px-6">
               <p className="rulesInfoHeader">Rank checker</p>
               <p className="text-gray-900 dark:text-white text-[18px] mt-2">
-                Check to see, if you&apos;re allowed to team up with a specific player!
+                Check to see, if you&apos;re allowed to team up with a specific
+                player!
               </p>
             </div>
             <header className="relative z-20 flex items-center justify-center border-gray-200 py-4 px-6 lg:flex-none">
@@ -86,36 +90,36 @@ export default function Team() {
                       className="mt-1 block w-full border border-gray-300 dark:border-[#242424] dark:bg-[#161616] rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-sky-500 focus:border-sky-500 sm:text-sm"
                     />
                   </div>
-                  
+
                   <div className="col-span-12 sm:col-span-6">
-                  <label
-                    htmlFor="scoresaber-id2"
-                    className="block text-sm font-medium text-gray-700 dark:text-white"
-                  >
-                    ScoreSaber ID for Player 2
-                  </label>
-                  <input
-                    type="number"
-                    name="scoresaber-id2"
-                    id="scoresaber-id2"
-                    onChange={(e) => setId2(e.target.value)}
-                    className="mt-1 block w-full border border-gray-300 dark:border-[#242424] dark:bg-[#161616] rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-sky-500 focus:border-sky-500 sm:text-sm"
-                  />
+                    <label
+                      htmlFor="scoresaber-id2"
+                      className="block text-sm font-medium text-gray-700 dark:text-white"
+                    >
+                      ScoreSaber ID for Player 2
+                    </label>
+                    <input
+                      type="number"
+                      name="scoresaber-id2"
+                      id="scoresaber-id2"
+                      onChange={(e) => setId2(e.target.value)}
+                      className="mt-1 block w-full border border-gray-300 dark:border-[#242424] dark:bg-[#161616] rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-sky-500 focus:border-sky-500 sm:text-sm"
+                    />
                   </div>
-                <div className="flex justify-end mt-5">
-                  <button
-                  onClick={handleCheck}
-                    className="bg-[#B97EF5] border border-transparent rounded-md shadow-sm py-2 px-4 inline-flex justify-center text-sm font-medium text-white hover:bg-[#8f5dc2] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500"
-                  >
-                    Check!
-                  </button>
+                  <div className="flex justify-end mt-5">
+                    <button
+                      onClick={handleCheck}
+                      className="bg-[#B97EF5] border border-transparent rounded-md shadow-sm py-2 px-4 inline-flex justify-center text-sm font-medium text-white hover:bg-[#8f5dc2] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500"
+                    >
+                      Check!
+                    </button>
+                  </div>
                 </div>
               </div>
+            </header>
           </div>
-        </header>
+        </>
       </div>
-    </>
-      </div >
     </>
   );
 }
