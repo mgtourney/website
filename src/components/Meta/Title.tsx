9fab006f4e8adb7fd58646b653bfeb5bb7bbf912
Checksum <checksum@velvet.moe>
2023-03-12 17:16:21 -0500

    style: update site metadata for richer embeds

--- src/components/Meta/Title.tsx
+++ src/components/Meta/Title.tsx
@@ -12,7 +12,6 @@ export default function Header({
   contents?: string;
 }) {
   const picture = image ? image : "/assets/images/base/Logo.png";
-  const description = contents ? contents : `${process.env.PUBLIC_NAME}`;
   const url = link ? link : `${process.env.PUBLIC_URL}`;
   return (
     <>
@@ -22,11 +21,15 @@ export default function Header({
       </Head>
       <meta property="og:image" content={picture} />
       <meta property="og:image:secure_url" content={title} />
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={description} />
+      <meta property="og:title" content='Magnesium' />
+      <meta property="og:description" content='Magnesium is an organization committed to creating and hosting exciting events and tournaments for gamers of all levels. Join us to showcase your skills, build lasting connections, and be a part of our thriving gaming community.' />
       <meta property="og:type" content="website" />
       <meta property="og:url" content={url} />
       <meta property="og:site_name" content={process.env.PUBLIC_NAME} />
+      <meta property="og:locale" content="en_US" />
+
+      <meta name="twitter:site" content="@mgtourneybs" />
+      <meta name="theme-color" content="#7B52A4" />
     </>
   );
 }
